99d6486b0f4738402cec31612a9dd18586ac5570
jayverner <jaylenverner@gmail.com>
2026-05-31 08:33:04 -0500

    Add files via upload

---
+++ dimensionalarray.js
@@ -0,0 +1,14 @@
+let allScores = new Array(3);
+
+
+let cheaterSeating = [
+  ["A1", "A2", "A3", "A4"], 
+  ["B1", "B2", "B3", "B4"],
+  ["C1", "C2", "C3", "C4"] 
+];
+
+let contactList = [ 
+["Bob", 3147720366, "bob555@gmail.com" ],
+["Jen", 3141277673, "jen12@gmail.com"],
+["Walter", 3145909485, "walterw0@gmail.com"]
+];
